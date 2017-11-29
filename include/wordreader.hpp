0f3e30740c5323a81d5670c784e4a6636363e224
Jonathan F <fungjys@gmail.com>
2017-11-29 19:59:46 +1000

    Swapped BIG and LITTLE endian around, I had it the wrong way around.

--- include/wordreader.hpp
+++ include/wordreader.hpp
@@ -12,25 +12,25 @@ namespace wordreader
     };
 
 
-    template <typename T>
-    T readword(int8_t first, int8_t second, endian_t type  = BIG_ENDIAN)
+    template <typename ReturnType>
+    ReturnType readword(int8_t first, int8_t second, endian_t type  = BIG_ENDIAN)
     {
-        if (type == BIG_ENDIAN)
-        {
-            // The most significant byte is at the lowest memory address.
-            // ie, 0xHHLL, where H is the high byte and L is the low byte.
-           return ((first << 8) | second);
-        }
-        else if (type == LITTLE_ENDIAN)
+        if (type == BIG)
         {
 
             // The least significant byte is in the highest memory address.
             // ie, 0xLLHH, where L is the low byte and H is the high byte.
             return ((second << 8) | (first));
         }
+        else if (type == LITTLE)
+        {
+            // The most significant byte is at the lowest memory address.
+            // ie, 0xHHLL, where H is the high byte and L is the low byte.
+           return ((first << 8) | second);
+        }
         else
         {
-            return (T)0;
+            return (ReturnType)0;
         }
 
     }
