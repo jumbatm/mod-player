8ca0f105b75771a3e54fd5442167233bed76b3d6
Jonathan F <fungjys@gmail.com>
2017-11-29 21:23:20 +1000

    Reverted change to BIG and LITTLE endianness.

--- include/wordreader.hpp
+++ include/wordreader.hpp
@@ -12,25 +12,25 @@ namespace wordreader
     };
 
 
-    template <typename ReturnType>
-    ReturnType readword(int8_t first, int8_t second, endian_t type  = BIG_ENDIAN)
+    template <typename T>
+    T readword(int8_t first, int8_t second, endian_t type  = BIG_ENDIAN)
     {
-        if (type == BIG)
+        if (type == BIG_ENDIAN)
+        {
+            // The most significant byte is at the lowest memory address.
+            // ie, 0xHHLL, where H is the high byte and L is the low byte.
+           return ((first << 8) | second);
+        }
+        else if (type == LITTLE_ENDIAN)
         {
 
             // The least significant byte is in the highest memory address.
             // ie, 0xLLHH, where L is the low byte and H is the high byte.
             return ((second << 8) | (first));
         }
-        else if (type == LITTLE)
-        {
-            // The most significant byte is at the lowest memory address.
-            // ie, 0xHHLL, where H is the high byte and L is the low byte.
-           return ((first << 8) | second);
-        }
         else
         {
-            return (ReturnType)0;
+            return (T)0;
         }
 
     }
