bb693ffb0511832031ad00275b7885d44d8d8c77
Jonathan F <fungjys@gmail.com>
2017-12-04 10:15:04 +1000

    Temporarily commented out pa_strerror - it's causing a strange linking error which I'll have to investigate.

--- soundplayer/soundplayer.cpp
+++ soundplayer/soundplayer.cpp
@@ -1,7 +1,7 @@
 #include "soundplayer.hpp"
 
 #include <pulse/simple.h>
-#include <pulse/error.h>
+//#include <pulse/error.h>
 
 #include <exception>
 #include <iostream> // TODO: (See line 39).
@@ -16,7 +16,7 @@ namespace SoundPlayer
         // Connect to the PulseAudio sound server.
 
         sampleSpec.format = PA_SAMPLE_U8; // Use unsigned 8 bit PCM
-        sampleSpec.sampleRate = sampleRate_;
+        sampleSpec.rate = sampleRate_;
         sampleSpec.channels = 1;
 
         // Must remember to static_cast<unsigned char> before feeding to PulseAudio.
@@ -37,19 +37,21 @@ namespace SoundPlayer
 
         if (!soundServer)
         {
-            std::cout << pa_strerror(error) << std::endl; // TODO: Format this into the exception string.
-            throw std::runtime_exception("Failed to make soundServer.");
+            //std::cout << pa_strerror(error) << std::endl; // TODO: Format this into the exception string.
+            throw std::runtime_error("Failed to make soundServer.");
         }
 
     }
 
     void playRaw(int8_t *rawData, unsigned size)
     {
+        int error;
+
         pa_simple_write(soundServer, rawData, size, &error);
 
         if (error < 0)
         {
-           throw std::runtime_exception("Failed to play audio!");
+           throw std::runtime_error("Failed to play audio!");
         }
     }
 }
