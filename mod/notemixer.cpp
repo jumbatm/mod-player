143269147221f5f212b52b795c3932cdc32af6d1
Jonathan F <fungjys@gmail.com>
2018-04-22 17:38:08 +1000

    Add tests for Note class

--- mod/notemixer.cpp
+++ mod/notemixer.cpp
@@ -1,48 +1,9 @@
 #include "mod/notemixer.hpp"
-#include "mod/effect.hpp"
+#include "mod/note.hpp"
+
 #include "wordreader.hpp"
 
 #include <iostream>
-
-constexpr bool USE_PAL_VALUE = true;
-
-namespace
-{
-    class Note
-    {
-        private:
-            uint8_t m_sampleNumber = 0;
-            uint16_t m_period = 0;
-            effect_t m_effect = SILENCE;
-            uint8_t m_argument = 0;
-        public:
-            Note(uint32_t d)
-            {
-                uint8_t *data = reinterpret_cast<uint8_t *>(&d);
-                m_sampleNumber = (data[0] & (0xF << 4)) | ((data[2] & 0xF) >> 4);
-                m_period = ((data[0] & 0xF) << 8) | data[1];
-                m_effect = static_cast<effect_t>(data[2] & 0xF);
-                m_argument = data[3];
-            }
-
-            double sampleRate()
-            {
-                double multiplier;
-
-                if constexpr(USE_PAL_VALUE)
-                {
-                    multiplier = 7093789.2;
-                } 
-                else 
-                {
-                    multiplier = 7159090.2;
-                }
-
-                return multiplier / (2 * m_period);
-            }
-
-    };
-}
 
 NoteMixer NoteMixer::Create(Sample& samp, uint32_t d)
 {
