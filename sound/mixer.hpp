5cf6307f77aa04bd31540a7c51f702d1ced73a7e
Jonathan F <fungjys@gmail.com>
2018-06-30 21:27:39 +1000

    Use a wrapped valarray approach for Mixer instead of a singleton

--- sound/mixer.hpp
+++ sound/mixer.hpp
@@ -2,21 +2,54 @@
 
 #include <cstdint>
 #include <cstddef>
+#include <valarray>
+#include <limits>
 
-namespace Mixer
+template <typename ElementType>
+class Mixer
 {
-    struct TrackInfo
+    // So it turns out we can't just naively take an average, because if the
+    // other track is full of zeroes, we end up hearing the first track, but at
+    // half amplitude. Instead, we use equation described from
+    // http://www.vttoth.com/CMS/index.php/technical-notes/68. 
+    // Z = A + B - (AB/256).
+    //
+    // This class simply wraps a valarray and makes it perform this operation on
+    // its addition and subtraction operators, . For ease of use, it also provides
+    // iterator support.
+ 
+    public:
+    // Default construct.
+    Mixer() : data()
     {
-        uint8_t const* data;
-        size_t const size;
-    };
-
-    namespace helper
-    {
-        size_t scale(size_t index, double scalefactor);
     }
 
-    void newTrack(unsigned sampleSize); // Initialise a new track.
-    void mixIn(size_t offset, const uint8_t *const track, size_t size, double scalefactor = 1.0); // Mix in new track.
-    TrackInfo getTrack();
-}
+    ElementType& operator[](size_t index)
+    {
+        return data[index];
+    }
+
+    Mixer<ElementType>& operator+(const std::valarray<ElementType>& other)
+    {
+        return Mixer{data + other - (data * other) 
+            / static_cast<double>(std::numeric_limits<ElementType>::max())};
+        // See comment below.
+    }
+
+    // A helper function to apply the formula described above. Automatically
+    // performs scaling to the maximum value ResultType can hold.
+    template <typename Type, typename ResultType = Type>
+    static ResultType mix(Type firstValue, Type secondValue)
+    {
+        // The "AB/256" from the formula above, for any type. Casted to avoid
+        // integer division.
+        double normaliser = firstValue * secondValue
+            / static_cast<double>(std::numeric_limits<ResultType>::max());
+
+        return firstValue + secondValue - normaliser;
+    }
+
+
+    private:
+    std::valarray<ElementType> data;
+};
