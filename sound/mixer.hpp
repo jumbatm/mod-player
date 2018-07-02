0878d1464bb5d752448fe5390597250a039e06cf
Jonathan F <fungjys@gmail.com>
2018-07-02 13:50:01 +1000

    Provide mixing function rather than entire class.
    Providing just the function makes this more usable in almost every
    possible way.

--- sound/mixer.hpp
+++ sound/mixer.hpp
@@ -1,25 +1,12 @@
 #pragma once
 
-#include <valarray>
+#include <vector>
 #include <limits>
 
-template <typename ElementType>
-class Mixer
+namespace Mixer
 {
-    // So it turns out we can't just naively take an average, because if the
-    // other track is full of zeroes, we end up hearing the first track, but at
-    // half amplitude. Instead, we use equation described from
-    // http://www.vttoth.com/CMS/index.php/technical-notes/68. 
-    // Z = A + B - (AB/256).
-    //
-    // This class simply wraps a valarray and makes it perform this operation on
-    // its addition and subtraction operators, . For ease of use, it also provides
-    // iterator support.
- 
-    public:
-    // A helper function to apply the formula described above. Automatically
-    // performs scaling to the maximum value ResultType can hold.
-    static ElementType mix(ElementType firstValue, ElementType secondValue)
+    template <typename ElementType>
+    ElementType mix(ElementType firstValue, ElementType secondValue)
     {
         // The "AB/256" from the formula above, for any type. Casted to avoid
         // integer division.
@@ -29,33 +16,4 @@ class Mixer
         return firstValue + secondValue - normaliser;
     }
 
-    // Default construct.
-    Mixer() : data()
-    {
-    }
-
-    // Construct from a valarray.
-    Mixer(std::valarray<ElementType>&& toOwn) 
-        : data(std::move(toOwn))
-    {
-    }
-
-    ElementType& operator[](size_t index)
-    {
-        return data[index];
-    }
-
-    Mixer<ElementType>& operator+(const std::valarray<ElementType>& other)
-    {
-        // Unfortunately, valarray doesn't seem to provide a way to modify
-        // elements in-place. They have an `apply` method, but this, for all
-        // intents and purposes, generates a new valarray. Therefore, one of the
-        // tests for this class is to verify that this operator produces results
-        // identical to the one above.
-        return Mixer{data + other - (data * other) 
-            / static_cast<double>(std::numeric_limits<ElementType>::max())};
-    }
-
-    private:
-    std::valarray<ElementType> data;
-};
+} // end namespace Mixer::util
